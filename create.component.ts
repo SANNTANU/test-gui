9aae687d750734b458743c63e4652a91a13bc693
SANNTANU <43367675+SANNTANU@users.noreply.github.com>
2019-08-07 11:33:35 +0530

    Add files via upload

--- create.component.ts
+++ create.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Validators, FormControl, FormGroup, FormArray } from '@angular/forms';
-import { MessageService } from 'primeng/primeng';
+import { Validators, FormControl, FormGroup, AbstractControl, ValidationErrors } from '@angular/forms';
+import { MessageService, SelectItem } from 'primeng/primeng';
 import { EmployeesService } from '../employees.service';
-
+import { throwError, Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create',
@@ -11,69 +13,203 @@ import { EmployeesService } from '../employees.service';
   encapsulation: ViewEncapsulation.None
 })
 export class CreateComponent implements OnInit {
+  private minDate: Date;
+  private maxDate: Date;
+  private year: string;
 
-  private registrationForm: FormGroup;
-  private blockedPage: boolean = false;
-  private isEmailChecked: boolean = false;
+  private createEmployeeForm: FormGroup;
 
-  esiChecked: boolean = false;
-  disabled: boolean = true;
+  private designationSelect: SelectItem[];
+  private departmentSelect: SelectItem[];
+  private workingShiftSelect: SelectItem[];
 
-  designation: any[];
-  department: any[];
-  updatedregistrationForm: any[];
-
-  constructor(private messageService: MessageService, private employeesService: EmployeesService) { }
+  constructor(private messageService: MessageService, private employeesService: EmployeesService, private router: Router) { }
 
   reset() {
+    let today = new Date();
+    this.year = today.getFullYear().toString();
 
+    // NO USE AS OF NOW
+    // let month = today.getMonth();
+    // let prevMonth = (month === 0) ? 11 : month - 1;
+    // this.minDate = new Date();
+    // this.minDate.setMonth(prevMonth);
+
+    this.maxDate = new Date();
+
+    this.designationSelect = [{ label: "Select One", value: null }];
+    this.departmentSelect = [{ label: "Select One", value: null }];
+    this.workingShiftSelect = [
+      { label: 'Select One', value: null },
+      { label: 'Day Shift', value: 'D' },
+      { label: 'Night Shift', value: 'N' }
+    ];
   }
 
   ngOnInit() {
     this.reset();
 
-    this.employeesService.getDesignation().subscribe(
-      (data: any) => {
-        this.designation = data;
-        console.log('this.datasource ----- ' + JSON.stringify(this.designation));
-      });
+    this.createEmployeeForm = new FormGroup({
+      employeeName: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z].(([ -!][a-zA-Z-!])?[a-zA-Z]*)*$')]),
+      employeeCode: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.pattern('[SWD]+ [0-9]*')
+        ],
+        asyncValidators: [this.employeeCodeValidator.bind(this)],
+        updateOn: 'blur'
+      }),
+      dateOfJoining: new FormControl(null, Validators.required),
+      employeeEmail: new FormControl('', Validators.email),
+      employeePhone: new FormControl('', [Validators.minLength(10), Validators.maxLength(12), Validators.pattern('[1-9]+[0-9]*')]),
+      designationMstId: new FormControl(null, Validators.required),
+      departmentMstId: new FormControl(null, Validators.required),
+      workingShift: new FormControl(null, Validators.required),
+      leaveApplicableDateTime: new FormControl(null, Validators.required),
+      accountNo: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(18),
+          Validators.pattern('[0-9]*')
+        ],
+        asyncValidators: [this.accountNoValidator.bind(this)],
+        updateOn: 'blur'
+      }),
+      grossSalaryAmount: new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(6), Validators.pattern('[1-9]+[0-9]*')]),
 
-    this.employeesService.getDepartment().subscribe(
-      (data: any) => {
-        this.department = data;
-        console.log('this.datasource ----- ' + JSON.stringify(this.department));
-      });
+      isPFApplicable: new FormControl(false, this.pfAcNoValidator.bind(this)),
+      pfAcNo: new FormControl({ value: null, disabled: true }, {
+        validators: [
+          Validators.minLength(10),
+          Validators.maxLength(12),
+          Validators.pattern('[1-9]+[0-9]*')
+        ],
+        asyncValidators: [this.pfAcNoExistsValidator.bind(this)],
+        updateOn: 'blur'
+      }),
 
-    this.registrationForm = new FormGroup({
-      employeeName: new FormControl(null, Validators.required),
-      employeeCode: new FormControl(null, Validators.required),
-      dateOfJoining: new FormControl(null),
-      employeeEmail: new FormControl(null),
-      employeePhone: new FormControl(null),
-      designation: new FormControl(null, Validators.required),
-      department: new FormControl(null, Validators.required),
-      accountNo: new FormControl(null),
-      grossSalaryAmount: new FormControl(null, Validators.required),
-      pfAcNo: new FormControl(null),
-      esiAcNo: new FormControl(null),
-      designationMstId: new FormControl(),
-      departmentMstId: new FormControl()
+      isESIApplicable: new FormControl(false, this.esiAcNoValidator.bind(this)),
+      esiAcNo: new FormControl({ value: null, disabled: true }, {
+        validators: [
+          Validators.minLength(10),
+          Validators.maxLength(17),
+          Validators.pattern('[1-9]+[0-9]*')
+        ],
+        asyncValidators: [this.esiAcNoExistsValidator.bind(this)],
+        updateOn: 'blur'
+      })
     });
+
+    this.employeesService.getDesignations().subscribe(
+      (data: any) => {
+        if (data) {
+          data.forEach(obj => {
+            this.designationSelect.push({ label: obj.designationName, value: obj.id })
+          });
+        }
+      },
+      (error) => {
+        return throwError(error);
+      });
+
+    this.employeesService.getDepartments().subscribe(
+      (data: any) => {
+        if (data) {
+          data.forEach(obj => {
+            this.departmentSelect.push({ label: obj.departmentName, value: obj.id })
+          });
+        }
+      },
+      (error) => {
+        return throwError(error);
+      });
   }
 
-  onSubmit(registrationFormVal: any) {
-    this.registrationForm.patchValue({ designationMstId: this.registrationForm.value.designation.id });
-    this.registrationForm.patchValue({ departmentMstId: this.registrationForm.value.department.id });
-
-    this.employeesService.createEmployee(this.registrationForm.value).subscribe(
+  onSubmit(createEmployeeFormVal: any) {
+    this.employeesService.createEmployee(createEmployeeFormVal).subscribe(
       (data: any) => {
-        console.log('data response....' + data);
         if (data = true) {
-          console.log('toast working........');
-          this.messageService.add({ key: 'tc', severity: 'info', summary: 'Message', detail: 'Employee details saved successfully.' });
+          this.showMessage('success', true, false, 'success', 'Success', 'Employee saved successfully.');
         }
+        else {
+          this.showMessage('error', false, true, 'error', 'Error', 'Employee save unsuccessful.');
+        }
+      },
+      (error) => {
+        this.showMessage('error', false, true, 'error', 'Error', 'Employee save unsuccessful.');
+        return throwError(error);
       }
     );
-    console.log("data......." + JSON.stringify(this.registrationForm.value));
+  }
+
+  showMessage(messageKey: string, messageSticky: boolean, messageClosable: boolean, messageSeverity: string, messageSummary: string, messageDetail: string) {
+    this.messageService.add({ key: messageKey, sticky: messageSticky, closable: messageClosable, severity: messageSeverity, summary: messageSummary, detail: messageDetail });
+  }
+
+  closeDialog() {
+    this.reset();
+    this.router.navigate(['/dashboard/employees/view-delete']);
+  }
+
+  /* ==================================================================================================================== */
+
+  employeeCodeValidator(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    return this.employeesService.checkEmployeeCode({ "employeeCode": control.value }).pipe(
+      map(data => {
+        return (data && data == true) ? { "employeeCodeExists": true } : null;
+      })
+    );
+  }
+
+  accountNoValidator(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    return this.employeesService.checkAccountNo(control.value).pipe(
+      map(data => {
+        return (data && data == true) ? { "accountNoExists": true } : null;
+      })
+    );
+  }
+
+  pfAcNoValidator(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    if (control.value === true) {
+      this.createEmployeeForm.controls.pfAcNo.enable();
+    } else {
+      if (this.createEmployeeForm != undefined) {
+        this.createEmployeeForm.controls.pfAcNo.reset();
+        this.createEmployeeForm.controls.pfAcNo.disable();
+      }
+    }
+
+    return null;
+  }
+
+  esiAcNoValidator(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    if (control.value === true) {
+      this.createEmployeeForm.controls.esiAcNo.enable();
+    } else {
+      if (this.createEmployeeForm != undefined) {
+        this.createEmployeeForm.controls.esiAcNo.reset();
+        this.createEmployeeForm.controls.esiAcNo.disable();
+      }
+    }
+
+    return null;
+  }
+
+  pfAcNoExistsValidator(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    return this.employeesService.checkPfAcNo(control.value).pipe(
+      map(data => {
+        console.log('data', data);
+        return (data && data == true) ? { "pfAccountNoExists": true } : null;
+      })
+    );
+  }
+
+  esiAcNoExistsValidator(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    return this.employeesService.checkEsiAcNo(control.value).pipe(
+      map(data => {
+        return (data && data == true) ? { "esiAccountNoExists": true } : null;
+      })
+    );
   }
 }
